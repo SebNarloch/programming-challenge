603bff0f635c80409518c2c8f15cbdc6bf301f92
SebNarloch <sebastian.narloch98@gmail.com>
2025-04-16 09:34:25 +1000

    fix: fixed js doc

--- js/programming-challenge.ts
+++ js/programming-challenge.ts
@@ -7,19 +7,19 @@ const phrases = {
   "Good morning": "Oodgay orningmay",
   "How are you": "Owhay areway ouyay",
   "Coffee is life": "Offeecay isway ifelay",
+  "I love football": "Iway ovelay ootballfay",
   "Creativity meets code": "Eativitycray eetsmay odecay",
+  "A mouse is a small animal": "Away ousemay isway away allsmay animalway",
 };
 
 /*
  * Rules of Pig Latin:
- * 1. For words that begin with a vowel sound (a, e, i, o, u):
+ * 1. For words that begin with a vowel:
  *    - Add "way" to the end of the word.
- *    - Example: "apple" becomes "appleway".
  *
- * 2. For words that begin with a consonant sound:
+ * 2. For words that begin with a consonant:
  *    - Move the leading consonant or consonant cluster to the end of the word,
  *      then append "ay" to the end.
- *      Example: "hello" becomes "ellohay".
  */
 
 // Vowels constant
@@ -40,13 +40,21 @@ function translateEnglishToPigLatin(englishPhrase: string) {
     const charsInWord = word.split("");
     const firstChar = charsInWord[0];
 
-    if (vowels.includes(firstChar)) {
+    if (vowels.includes(firstChar.toLowerCase())) {
       return word + "way";
     } else {
       // find the first index of a vowel in a word
       const firstVowelIndex = charsInWord.findIndex((char) =>
         vowels.includes(char.toLowerCase())
       );
+
+      // if there are no vowels in the word, just add 'ay' to the end
+      if (firstVowelIndex === -1) {
+        return charsInWord.push("a", "y");
+        // handling single vowel words like "I", "a", etc.
+      } else if (firstVowelIndex === 0 && charsInWord.length === 1) {
+        return charsInWord.push("w", "a", "y");
+      }
 
       // move the consonant cluster to the end of the word and add 'ay'
       const consonantCluster = charsInWord.splice(0, firstVowelIndex);
